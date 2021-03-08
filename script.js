c42269c82c7528a4868b49f432162d785e9ae551
kioksianto <brenda.sianto@gmail.com>
2021-03-08 04:25:55 +0300

    navigation and home

---
+++ script.js
@@ -0,0 +1,10 @@
+/*function on() {
+    document.getElementById("overlay").style.display = "nav-link-wrapper";
+  }*/
+
+$(document).ready(function(){
+    $('#overlay')(function(){
+        $('#overlay').show();
+
+
+});
