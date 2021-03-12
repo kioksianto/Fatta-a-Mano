3ef7f5e978384b72c2f5c26c4c9c53e6941c6f04
kioksianto <brenda.sianto@gmail.com>
2021-03-12 09:00:15 +0300

    updated CSS

--- script.js
+++ script.js
@@ -1,10 +1,63 @@
-/*function on() {
-    document.getElementById("overlay").style.display = "nav-link-wrapper";
-  }*/
+function placeOrder() { //function for placing an order
+  //get inputs from form
+  let name = capitalize_inputs($("input#name").val());
+  let flavor = $("#pizza-flavor").val();
+  let size = $("#pizza-size").val();
+  let crust = $("#pizza-crust").val();
+  let number = $("#pizza-number").val();
 
-$(document).ready(function(){
-    $('#overlay')(function(){
-        $('#overlay').show();
+  let sizeCost; 
+  //set different prices for the different pizza flavors depending on their size
+  if (flavor === "Hawaiian" || flavor === "Margharita" || flavor === "Marinara" || flavor === "Mushroom" || flavor === "Veggie" 
+  || flavor === "Oyster"|| flavor === "Pepperoni" || flavor === "Pulled Pork"|| flavor === "Spicy Veggie") {
+      if (size === "Larger than life") {
+          sizeCost = 1050;
+      } else if (size === "Not too big") {
+          sizeCost = 850;
+      } else if (size === " A bite") {
+          sizeCost = 450;
+      }
+    }
+    var crustCost; 
+    //set prices for different crust types
+    if (crust === "Gluten Free") {
+        crustCost = 230;
+    } else if (crust === "Stuffed") {
+        crustCost = 200;
+    } else if (crust === "Crusty") {
+        crustCost = 150;
+    } else if (crust === "None") {
+        crustCost = 180;
+    } 
 
+    //price calculation
+    let price = (sizeCost + crustCost);
+    let totalPrice = parseInt(price * number);
 
-});
+   
+    $('#deliveryoption','#deliveryconfirmation','#price').hide();
+    (function(){
+        $('#placeorder').click();
+        $('deliveryoption').show();
+    });
+
+    $('#inhouse').click(function(){
+        $('#price').show();
+        $('deliveryoption').hide();
+
+    });
+
+    $('#delivery').click(function(){
+        $('#deliveryconfirmation').show();
+        $('deliveryoption').hide();
+        var location = capitalize_inputs($("input#location").val()); 
+        //get delivery details
+        $(".location").text(location);
+    });
+
+    $('#checkout').click(function(){
+        $('#price').show();
+        $('deliveryconfirmation').hide();
+    });
+
+}
