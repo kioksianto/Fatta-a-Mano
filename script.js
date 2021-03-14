38dd998e3b6d735e53935dd255665cfe2eafc4a1
kioksianto <brenda.sianto@gmail.com>
2021-03-15 02:08:10 +0300

    transitioning pages

--- script.js
+++ script.js
@@ -1,157 +1,102 @@
-
-function PizzaOrder (flavor, size, crust, quantity){
-    this.size = size
-    this.crust = crust
-    this.number = number
-    this.flavor = flavor
+function PizzaOrder(name,flavour, size, crust, toppings, number){
+  this.name = name
+  this.size = size
+  this.crust = crust
+  this.number = number
+  this.flavor = flavour
+  this.toppings = toppings
 }
-
-let sizeCost; 
-  //set different prices for the different pizza flavors depending on their size
-  PizzaOrder.prototype.sizeCost = function(){
-    if (flavor === "Hawaiian" || flavor === "Margharita" || flavor === "Marinara" || flavor === "Mushroom" || flavor === "Veggie" 
-    || flavor === "Oyster"|| flavor === "Pepperoni" || flavor === "Pulled Pork"|| flavor === "Spicy Veggie") {
-        if (size === "Larger than life") {
-            sizeCost = 1050;
-        } else if (size === "Not too big") {
-            sizeCost = 850;
-        } else if (size === " A bite") {
-            sizeCost = 450;
-        }
-      }
-  }
-  PizzaOrder.prototype.crustCost = function(){
-  
-    var crustCost; 
-    //set prices for different crust types
-    if (crust === "Gluten Free") {
-        crustCost = 230;
-    } else if (crust === "Stuffed") {
-        crustCost = 200;
-    } else if (crust === "Crusty") {
-        crustCost = 150;
-    } else if (crust === "None") {
-        crustCost = 0;
-        
-    } 
-
-  }
-let totalCost;
-  PizzaOrder.prototype.totalCost = function(sizeCost, crustCost){
-
-    grandCost = sizeCost + crustCost
-  }
-  console.log (totalCost);
-
-
 $(document).ready(function() {
+
+  //placeorder button
   $("#orders").submit(function(event){
         event.preventDefault();
-let name = capitalize_inputs($("#name").val());
-let flavor = $("#pizza-flavor option:selected").val();
-let size = $("#pizza-size option:selected").val();
-let crust = $("#pizza-crust option:selected").val();
-let number = $("#pizza-number").val();
+let inputtedName = $("#name").val();
+let inputtedFlavour = $("#flavor option:selected").val();
+let inputtedSIze = $("#size option:selected").val();
+let inputtedCrust = $("#crust option:selected").val();
+let inputtedToppings = $("#toppings option:selected").val();
+let inputtedNumber = $("#quantity").val();
 
-console.log (name)
+// switch(inputtedSize){
+//   case "0": price = 0;
+//   break;
+//   case "larger than life": price = 1050;
+//   console.log(price);
+//   break;
+//   case "Not too big": price = 800;
+//   console.log(price);
+//   break;
+//   case "A bite": price = 450;
+//   console.log(price);
+//   default: console.log("error")
+// }
+// switch(inputtedCrust){
+//   case "0": crustPrice = 0;
+//   break;
+//   case "Gluten Free": crustPrice = 100;
+//   console.log(crustPrice);
+//   break;
+//   case "Stuffed": crustPrice = 150;
+//   console.log(crustPrice);
+//   break;
+//   case "Crusty": crustPrice = 250;
+//   console.log(crustPrice);
+//   default: console.log("No Price")
+// }
+
+// if ((pizzaSize == "0")){
+//   console.log("Nothing has been selected");
+
+  
+// }
+// else{
+//   $("button#placeorder").hide();
+//   $(".container-4").slideDown(1000);
+  
+// }
+// total = price + crustPrice;
+// console.log(total)
+// let checkoutTotal = 0;
+// checkoutTotal = checkoutTotal + total;
+
+
+// $("#customer-name").html($("#name option:selected").val());
+// $("#pizza-flavor").html($("#flavor option:selected").val());
+// $("#pizza-size").html($("#size option:selected").val());
+// $("#pizza-toppings").html("#toppings option:selected").val());
+// $("#pizza-price").html(total);
+
+
+console.log(inputtedCrust,inputtedName,inputtedFlavour,inputtedNumber, inputtedSIze, inputtedToppings)
+
+var newOrder = new PizzaOrder(inputtedName, inputtedFlavour, inputtedSIze, inputtedCrust, inputtedToppings, inputtedNumber);
+$(".container-4").slideDown(1000);
+
+$("#cart-items").append("<tr><td id ='customer-name'>"+newOrder.name+"</td> <td id ='pizza-flavor'>"+newOrder.flavor+"</td><td id ='Pizza-size'>" +newOrder.size+ "</td><td id ='pizza-toppings'>" +newOrder.toppings+"</td><td id ='pizza-crust'>"  +newOrder.crust+ "</td><td id ='pizza-quantity'>" +newOrder.number+"</td><td id ='pizza-price'>"   +newOrder.price+ "</td</tr>");
+
+console.log (newOrder);
+
+//deliveryOptions
+$("button#inhouse").click(function(){
+  $("button#placeorder").hide();
+  $(".container-4").hide();
+  $(".container-6").slideDown(1000);
+});
+
+$("button#delivery").click(function(){
+  $("button#placeorder").hide();
+  $(".container-4").hide();
+  $(".container-5").slideDown(1000);
+});
+
+$("button#checkout").click(function(){
+  $("button#placeorder").hide();
+  $(".container-5").hide();
+  $(".container-6").slideDown(1000);
+});
+
+
 })})
 
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function placeOrder() {  //function for placing an order
-//   //get inputs from form
-//   $(document).ready(function(event){ event.preventDefault()
-
-//   let name = capitalize_inputs($("#name").val());
-//   let flavor = $("#pizza-flavor option:selected").val();
-//   let size = $("#pizza-size option:selected").val();
-//   let crust = $("#pizza-crust option:selected").val();
-//   let number = $("#pizza-number").val();
-//   console.log(size);
-
-//   let sizeCost; 
-//   //set different prices for the different pizza flavors depending on their size
-//   if (flavor === "Hawaiian" || flavor === "Margharita" || flavor === "Marinara" || flavor === "Mushroom" || flavor === "Veggie" 
-//   || flavor === "Oyster"|| flavor === "Pepperoni" || flavor === "Pulled Pork"|| flavor === "Spicy Veggie") {
-//       if (size === "Larger than life") {
-//           sizeCost = 1050;
-//       } else if (size === "Not too big") {
-//           sizeCost = 850;
-//       } else if (size === " A bite") {
-//           sizeCost = 450;
-//       }
-//     }
-//     var crustCost; 
-//     //set prices for different crust types
-//     if (crust === "Gluten Free") {
-//         crustCost = 230;
-//     } else if (crust === "Stuffed") {
-//         crustCost = 200;
-//     } else if (crust === "Crusty") {
-//         crustCost = 150;
-//     } else if (crust === "None") {
-//         crustCost = 180;
-//     } 
-
-//     //price calculation
-//     let price = (sizeCost + crustCost);
-//     let totalPrice = parseInt(price * number);
-
-// //    $(document).ready(function(){ 
-      
-//     // $('#deliveryoption','#deliveryconfirmation','#price').hide();
-//     (function(){
-//         $('#placeorder').click();
-//         $('#deliveryoption').show();
-//     });
-
-//     $('#inhouse').click(function(){
-//         $('#price').show();
-//         $('#deliveryoption').hide();
-
-//     });
-
-//     $('#delivery').click(function(){
-//         $('#deliveryconfirmation').show();
-//         $('#deliveryoption').hide();
-//         var location = capitalize_inputs($("input#location").val()); 
-//         //get delivery details
-//         $(".location").text(location);
-//     });
-
-//     $('#checkout').click(function(){
-//         $('#price').show();
-//         $('#deliveryconfirmation').hide();
-//     });
-
-
-//    })
-// }
